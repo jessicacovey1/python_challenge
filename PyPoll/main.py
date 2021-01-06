73a30f69a1aa9c8adca0470a145f9e05271725f8
Jessica <arizonajess@gmail.com>
2021-01-05 21:58:31 -0700

    final poll update 9:58

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,12 @@
 import os
 import csv
 
-csvpath = os.path.join(".","Resources", "election_data.csv")
+csvpath = os.path.join("Resources", "election_data.csv")
 
 votes = 0
 winner = ""
+winner_votes=0
 candidates = ""
-percentage = 0
-new = True
 
 candidates = {}
  
@@ -16,31 +15,27 @@ with open (csvpath, 'r') as csvfile:
         csvheader = next(csvreader)
         
         for row in csvreader:
+           votes = votes + 1      #for every new row add 1 to the count variable 'votes' to get total votes
            
-           for name in candidates:
+           if row[2] in candidates:   #if the key name in column 3 is in candidate dictionary
+                candidates[row[2]] = candidates[row[2]] + 1  #add one to the key value count
 
-            if row[2] == name:
-                new = False
-                candidates[name] = candidates[name] + 1
-            # * The total number of votes cast
-            #print (votes)
+           else: 
+               candidates[row[2]] = 1         #else the candidate key in dictionary is one
+           #for name in candidates:
 
-        if new == True:
-            candidates[row[2]] = 1
-      
-             
+            #list of candidates that received votes   loop through dictionary and print out 
 
-        for key, value in candidates.items():
-            if value > # value is total votes for thtat candidate -not sure greater than other candidates total votes then
-                    #winner = key
-
-        #print (f"{key}: + {value}")
 
 print ("Election Results")
 print ('-------------------------')          
 print (f"Total Votes: {votes}")
 print ('-------------------------') 
-print (f"{key}: + {percentage} + {value}")
+for key, value in candidates.items():  #pulls out key and value and returns them
+    print (f"{key}: {100*value/votes} {value}")
+    if value > winner_votes:
+        winner = key 
+        winner_votes = value    
 print ('-------------------------')
 print (f"Winner: {winner}")
 
@@ -50,32 +45,12 @@ with open(output_path, 'w') as txt_file:
     txt_file.write ("Election Results")
     txt_file.write ('-------------------------')          
     txt_file.write (f"Total Votes: {votes}")
-    txt_file.write ('-------------------------') 
-    txt_file.write (f"{key}: + {percentage} + {value}")
+    txt_file.write ('-------------------------')
+    for key, value in candidates.items():  
+        txt_file.write (f"{key}: {100*value/votes} {value}")
+        if value > winner_votes:
+            winner = key 
+            winner_votes = value 
     txt_file.write ('-------------------------')
     txt_file.write (f"Winner: {winner}")
   
-
-
-    
-#The total number of votes cast
-
-# * A complete list of candidates who received votes
-
-# * The percentage of votes each candidate won
-
-# * The total number of votes each candidate won
-
-# * The winner of the election based on popular vote.
-
-# Election Results
-# -------------------------
-# Total Votes: 3521001
-# -------------------------
-# Khan: 63.000% (2218231)
-# Correy: 20.000% (704200)
-# Li: 14.000% (492940)
-# O'Tooley: 3.000% (105630)
-# -------------------------
-# Winner: Khan
-# -------------------------
